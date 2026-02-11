22a6f4743ceb4152a63e5477ab1fe753161937d8
checksum-ai <114566276+checksum-ai[bot]@users.noreply.github.com>
2026-02-10 17:26:04 -0800

    changes

---
+++ checksum/tests/test feature/test - Dl7jp.checksum.spec.ts
@@ -0,0 +1,15 @@
+import { init } from "@checksum-ai/runtime";
+const { test, defineChecksumTest, login, expect, checksumAI } = init();
+
+test.describe("Example test", () => {
+  test.beforeEach(async ({ page }) => {
+    await login(page);
+  });
+
+  test(
+    defineChecksumTest("Navigate to home page", "Dl7jp"),
+    async ({ page }) => {
+      await page.goto("/");
+    }
+  );
+});
